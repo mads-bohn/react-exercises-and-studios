1c42cbea70d579c5a5e6640c9c3a1632c02ab9d5
mads-bohn <mady.bohn@gmail.com>
2024-03-11 17:11:33 -0500

    completed part 1

--- part1/exercises/src/components/BookList.jsx
+++ part1/exercises/src/components/BookList.jsx
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books I've Read";
+   let book1 = "https://upload.wikimedia.org/wikipedia/en/thumb/8/88/Uzumaki.jpg/220px-Uzumaki.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/61gQoqXgOqL._AC_UF1000,1000_QL80_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/91LlW46QU-L._AC_UF1000,1000_QL80_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Uzumaki" />
+         <img src={book2} alt="Feng Shui Modern" />
+         <img src={book3} alt="The Artist's Way" />
       </div>      
    );
 }
