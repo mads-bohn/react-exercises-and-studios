d8d24a99906f9ab59875fea70988a36dfb4e7fd6
mads-bohn <mady.bohn@gmail.com>
2024-03-16 00:36:46 -0500

    added json and created introduction

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About Crochet</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Crochet uses one hook to create elaborate slipknots</li>
+            <li>The most basic stitch is called a single crochet</li>
+            <li>You can crochet in rows, circles, or make 3D shapes</li>
          </ol>
       </div>
    );
